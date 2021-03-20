603e2285ac29b7f5c25836488f4574a3c6c2fdac
Liyuan Menküc <liyuan.menkuec@tu-dortmund.de>
2021-03-20 16:27:53 +0100

    Version1

---
+++ Dictionary.py
@@ -0,0 +1,30 @@
+import json
+from difflib import get_close_matches
+
+#Data von Wörterbuch öffnen
+data = json.load(open("data.json"))
+
+#ein eingegebenes Wort in Data zu suchen
+def translate(wordOfInput):
+    wordOfInput = wordOfInput.lower()
+    if wordOfInput in data:
+        return data[wordOfInput]
+    elif len(get_close_matches(wordOfInput, data.keys())) > 0:
+        yOrN = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(wordOfInput, data.keys())[0])
+        if yOrN == "Y":
+            return data[get_close_matches(wordOfInput, data.keys())[0]]
+        elif yOrN == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+#Start des Programmes
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # English-Thesaurus
+
+In diesem Repository entsteht ein englisches Wörterbuch, mit Console zu benutzen. 
+Wenn Benutzer ein ähnliches Wort wie richtiges  eingibt, kann die Machine automatisch das Wort analysiren und ein möglichst richtiges Wort suchen.
